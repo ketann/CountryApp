8fdbfad163fd395e1ea78c087f9581bdc59186b9
Ketan Nakum <dhaval@lionobytes.com>
2021-05-03 19:35:04 +0530

    Implement MVVM Architecture with CountryModel and ListViewModel

--- app/src/main/java/com/example/countryapp/model/CountryModel.java
+++ app/src/main/java/com/example/countryapp/model/CountryModel.java
@@ -1,4 +1,25 @@
 package com.example.countryapp.model;
 
 public class CountryModel {
+    String countryName;
+    String capital;
+    String flag;
+
+    public CountryModel(String countryName, String capital, String flag) {
+        this.countryName = countryName;
+        this.capital = capital;
+        this.flag = flag;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
 }
--- app/src/main/java/com/example/countryapp/viewmodel/ListViewModel.java
+++ app/src/main/java/com/example/countryapp/viewmodel/ListViewModel.java
@@ -1,4 +1,36 @@
 package com.example.countryapp.viewmodel;
 
-public class ListViewModel {
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.countryapp.model.CountryModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListViewModel extends ViewModel {
+
+    public MutableLiveData<List<CountryModel>> countries = new MutableLiveData<List<CountryModel>>();
+    public MutableLiveData<Boolean> countryLoadError = new MutableLiveData<Boolean>();
+    public MutableLiveData<Boolean> loading = new MutableLiveData<Boolean>();
+
+    public void refresh() {
+        fetchCountries();
+    }
+
+    private void fetchCountries() {
+        CountryModel country1 = new CountryModel("Albania", "Tirana", "");
+        CountryModel country2 = new CountryModel("Brazil", "Brasilia", "");
+        CountryModel country3 = new CountryModel("Czechia", "Praha", "");
+
+        List<CountryModel> list = new ArrayList<>();
+        list.add(country1);
+        list.add(country2);
+        list.add(country3);
+
+        countries.setValue(list);
+        countryLoadError.setValue(false);
+        loading.setValue(false);
+    }
+
 }
