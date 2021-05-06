3d96499ec51d47caefc6e641ba132a1537c11a13
Ketan Nakum <dhaval@lionobytes.com>
2021-05-06 23:34:03 +0530

    Implement MVVM Architecture with CountryListAdapter and bind ListViewModel and Layout design

--- app/src/main/java/com/example/countryapp/view/MainActivity.java
+++ app/src/main/java/com/example/countryapp/view/MainActivity.java
@@ -1,16 +1,84 @@
 package com.example.countryapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.countryapp.R;
+import com.example.countryapp.model.CountryModel;
+import com.example.countryapp.viewmodel.ListViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.swipeRefreshLayout)
+    SwipeRefreshLayout refreshLayout;
+
+    @BindView(R.id.countriesList)
+    RecyclerView countryList;
+
+    @BindView(R.id.list_error)
+    TextView listError;
+
+    @BindView(R.id.loading_view)
+    ProgressBar loadingView;
+
+    private ListViewModel viewModel;
+    private CountryListAdapter adapter = new CountryListAdapter(new ArrayList<>());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        viewModel = ViewModelProviders.of(this).get(ListViewModel.class);
+        viewModel.refresh();
+
+        countryList.setLayoutManager(new LinearLayoutManager(this));
+        countryList.setAdapter(adapter);
+
+        observerViewModel();
+
+    }
+
+    private void observerViewModel() {
+        viewModel.countries.observe(this, countryModels -> {
+            if (countryModels != null) {
+                countryList.setVisibility(View.VISIBLE);
+                adapter.updateCounties(countryModels);
+            }
+        });
+
+        viewModel.countryLoadError.observe(this, isError -> {
+            if (isError != null) {
+                listError.setVisibility(isError ? View.VISIBLE : View.GONE);
+            }
+        });
+
+        viewModel.loading.observe(this, isLoading -> {
+            if (isLoading != null) {
+                loadingView.setVisibility(isLoading ? View.VISIBLE : View.GONE);
+                if (isLoading)
+                {
+                    listError.setVisibility(View.GONE);
+                    countryList.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
